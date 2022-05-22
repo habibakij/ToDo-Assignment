7aa20b0e16283c2ee158b6dd67693b0fc6ca4d36
habibakij <adnanhabib454@gmail.com>
2022-05-22 15:30:26 +0600

    add home screen

--- lib/home.dart
+++ lib/home.dart
@@ -25,12 +25,62 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
 
-      body: Container()
+      body: _customBody(context),
+
 
     );
   }
 
+  Widget _customBody(BuildContext context){
+    return CustomScrollView(
+      slivers: [
 
+        SliverToBoxAdapter(
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: 15,
+            physics: const NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index){
+            return Card(
+              elevation: 1,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(color: Colors.red.withOpacity(.2), width: 1),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.all(5),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+
+                    Text(
+                      "Title $index",
+                      style: const TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                      ),
+                    ),
+
+                    Text(
+                      "Description $index",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey.withOpacity(.8),
+                      ),
+                    ),
+
+                  ],
+                ),
+              ),
+            );
+          },),
+        ),
+
+      ],
+    );
+  }
 
 
 }
